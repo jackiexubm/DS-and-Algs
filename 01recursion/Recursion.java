78be7e878b1ac34b8feefc6e8b6f76d52d3a9409
Jackie <jackiexubm1@gmail.com>
2016-02-05 10:20:35 -0500

    hw 01recursion

---
+++ 01recursion/Recursion.java
@@ -0,0 +1,17 @@
+public class Recursion{
+
+    public String name(){
+	return "Xu,Jackie";
+    }
+    
+    public double sqrt(double n){
+	
+	return helper(n, ); 
+    }
+
+    public double helper(double original, double current){
+	if((original - current)/original) >= .0000000001 && (original - current)/original <= .0000000001){
+	
+    }
+   
+}
