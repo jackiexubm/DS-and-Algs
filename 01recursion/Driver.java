75fe11751b707c5fe1ab7caec3915e2cfa258dc5
Jackie <jackiexubm1@gmail.com>
2016-02-05 11:58:31 -0500

    Add driver

---
+++ 01recursion/Driver.java
@@ -0,0 +1,8 @@
+public class Driver{
+    public static void main(String[]args){
+	Recursion a = new Recursion();
+	int i = 64;
+        a.sqrt(i);
+    }
+    
+}
--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -1,17 +1,20 @@
 public class Recursion{
 
+    public Recursion(){};
+
     public String name(){
 	return "Xu,Jackie";
     }
     
     public double sqrt(double n){
 	
-	return helper(n, ); 
+	return helper(n,n); 
     }
 
-    public double helper(double original, double current){
-	if((original - current)/original) >= .0000000001 && (original - current)/original <= .0000000001){
-	
+    public double helper(double original, double guess){
+	if((original - guess)/original >= .0000001 && (original - guess)/original <= .0000001){
+	    return guess;
+	}
+	return helper(original, (original / guess + guess) / 2);
     }
-   
 }
