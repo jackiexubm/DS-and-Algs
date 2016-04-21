85401998aa9e9b55aa0939d87df876b09bae228c
Jackie Xu <jackiexubm1@gmail.com>
2016-04-21 14:49:38 -0400

    complete height method

--- 12BSTree/BSTree.java
+++ 12BSTree/BSTree.java
@@ -1,9 +1,13 @@
 public class BSTree<T extends Comparable<T>>{
     private class Node{
-       T data;
-       Node left;
-       Node right;
-        
+        T data;
+        Node left;
+        Node right;
+
+        public Node(T d){
+            data = d;
+        }
+
         public T getData(){
             return data;
         }
@@ -12,11 +16,11 @@ public class BSTree<T extends Comparable<T>>{
             data = d;
         }
 
-        public Node getleft(){
+        public Node getLeft(){
             return left;
         }
 
-        public void setleft(Node l){
+        public void setLeft(Node l){
             left = l;
         }
 
@@ -28,53 +32,72 @@ public class BSTree<T extends Comparable<T>>{
             right = r;
         }
 
+        public int height(){
+            return heightHelper(this, 1);
+        }
+
+        private int heightHelper(Node n, int h){
+            if (n.getLeft() == null && n.getRight() == null) {
+                return 1;
+            }else if (n.getRight() == null) {
+                return heightHelper(n.left, h + 1);
+            }else if (n.getLeft() == null) {
+                return heightHelper(n.right, h + 1);
+            }
+            int l = heightHelper(n.left, h + 1);
+            int r = heightHelper(n.right, h + 1);
+            if (l > r) {
+                return l;
+            }
+            return r;
+        }
+
+        public void add(T value){
+
+        }
+
+        public String toString(){
+            return "";
+        }
+
+        public boolean contains(T value){
+            return containsHelper(root, value);
+        }
+
+        private boolean containsHelper(Node n, T value){
+            return false;
+        }
+    }
 
 
+    private Node root;
 
-
-
-       public int height(){
-        return heightHelper(this, 1);
-       }
-
-       private int heightHelper(Node n, int h){
-        if (n.left == null && right == null) {
-               return 1;
-           }else if (right == null) {
-               
-           }else if (left == null) {
-               
-           }
-           return 0;
-       }
-
-       public void add(T value){
-       }
-       public String toString(){
-           return "";
-       }
-       public boolean contains(T value){
-           return false;
-       }
-
-   }
-
-   private Node root;
+    public BSTree(){
+        root = new Node(null);
+    }
 
         //OUTER methods here are wrapper methods for the root
-   public int getHeight(){
+    public int getHeight(){
         //call the root's methods
         //check for empty first!
-       return root.height();
-   }
+        return root.height();
+    }
 
-   public void add(T value){
+    public void add(T value){
  //check for empty before you do things with root.
-   }
-   public String toString(){
+        if (root.getData() == null) {
+            root.setData(value);
+        }
+
+
+    }
+    public String toString(){
  //check for empty before you do things with root.
+
        return "";
    }
+
+
    public boolean contains(T value){
  //check for empty before you do things with root.
        return false;
---
+++ 12BSTree/Driver.java
@@ -0,0 +1,6 @@
+public class Driver{
+	public static void main(String[] args) {
+		BSTree a = new BSTree();
+		System.out.println(a.getHeight());
+	}
+}
