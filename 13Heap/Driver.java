ec946994c4646a16febf45b257831bfaf4a5bcab
Jackie Xu <jackiexubm1@gmail.com>
2016-05-09 14:00:12 -0400

    complete pushDown method

--- 13Heap/Driver.java
+++ 13Heap/Driver.java
@@ -3,8 +3,7 @@ public class Driver{
 		Integer[] b = {new Integer(1),new Integer(3),new Integer(6),new Integer(5),new Integer(9),new Integer(8), new Integer(-2)};
 		MyHeap<Integer> a = new MyHeap<Integer>(b,false);
 		System.out.println(a);
-		a.pushUp(7);
+		a.pushDown(7);
 		System.out.println(a);
-		
 	}
 }
--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -45,8 +45,57 @@ public class MyHeap<T extends Comparable<T>>{
  	//                     shifted to the correct spot.
  	//                     -data[k] and is a valid heap
 
-	private void pushDown(int k){
-
+	public void pushDown(int k){
+		if (Math.log(k) / Math.log(2) == Math.log(size) / Math.log(2)) {     // log base 2 of k
+			return;
+		}
+		if (isMax == true) {
+			if (getLeft(k) < size && getRight(k) < size) {   // has left and right children
+				if ( data[getLeft(k)].compareTo(data[k]) > 0 || data[getRight(k)].compareTo(data[k]) > 0 ) {
+					if (data[getLeft(k)].compareTo(data[getRight(k)]) > 0) {
+						T temp = data[k];
+						data[k] = data[getLeft(k)];
+						data[getLeft(k)] = temp;
+						pushDown(getLeft(k));
+					}else {
+						T temp = data[k];
+						data[k] = data[getRight(k)];
+						data[getRight(k)] = temp;
+						pushDown(getRight(k));
+					}
+				}
+			}else if (getLeft(k) <= size) {
+				if (data[getLeft(k)].compareTo(data[k]) > 0) {
+					T temp = data[k];
+					data[k] = data[getLeft(k)];
+					data[getLeft(k)] = temp;
+					pushDown(getLeft(k));
+				}
+			}
+		}else {
+			if (getLeft(k) < size && getRight(k) < size) {   // has left and right children
+				if ( data[getLeft(k)].compareTo(data[k]) < 0 || data[getRight(k)].compareTo(data[k]) < 0 ) {
+					if (data[getLeft(k)].compareTo(data[getRight(k)]) < 0) {
+						T temp = data[k];
+						data[k] = data[getLeft(k)];
+						data[getLeft(k)] = temp;
+						pushDown(getLeft(k));
+					}else {
+						T temp = data[k];
+						data[k] = data[getRight(k)];
+						data[getRight(k)] = temp;
+						pushDown(getRight(k));
+					}
+				}
+			}else if (getLeft(k) <= size) {
+				if (data[getLeft(k)].compareTo(data[k]) < 0) {
+					T temp = data[k];
+					data[k] = data[getLeft(k)];
+					data[getLeft(k)] = temp;
+					pushDown(getLeft(k));
+				}
+			}
+		}
 	}
 
 	public void pushUp(int k){
@@ -88,9 +137,11 @@ public class MyHeap<T extends Comparable<T>>{
 		return  s + data[size - 1] + "]";
 	}
 
-	private void heapify(){
-
-	}
+	// private void heapify(){
+	// 	for (int i = size/2;i > 0; i-- ) {
+	// 		pushDown(i);
+	// 	}
+	// }
 
 	private void doubleSize(){
 		T[] temp = (T[]) new Comparable[data.length * 2];
